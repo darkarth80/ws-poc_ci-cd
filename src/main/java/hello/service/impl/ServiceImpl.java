844498afd9f82bc1a66de1dff4f4b52e77f9dc60
darkarth80 <26891629+darkarth80@users.noreply.github.com>
2020-03-23 00:03:25 -0600

    Stereotyped and autowired ServiceImpl class.

--- src/main/java/hello/service/impl/ServiceImpl.java
+++ src/main/java/hello/service/impl/ServiceImpl.java
@@ -1,16 +1,28 @@
 package hello.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import hello.dto.Walmart;
 import hello.service.Service;
 
+@org.springframework.stereotype.Service
 public class ServiceImpl implements Service {
+
+    @Value("${app.paises.default.pais}")
+    private String pais;
+
+    @Value("${app.paises.default.oficina}")
+    private String oficina;
+
+    @Value("${app.paises.default.informacion}")
+    private String informacion;
 
 	@Override
 	public Walmart getInformacion() {
 		Walmart walmart = new Walmart();
-		walmart.setPais("MEX");
-		walmart.setOficina("Azcapotzalco");
-		walmart.setInformacion("POC");
+		walmart.setPais("US");
+		walmart.setOficina("Bentonville");
+		walmart.setInformacion("Information from BV");
 		return walmart;
 	}
 
